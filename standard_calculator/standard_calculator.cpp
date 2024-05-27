d8ce5cd3a906fadd69933aca94f3f0dcd2bf8b64
Emanuel Ruiz <emanuel.u.ruiz@gmail.com>
2024-05-27 12:44:08 -0700

    add partial methods to standard calculator class

--- standard_calculator/standard_calculator.cpp
+++ standard_calculator/standard_calculator.cpp
@@ -3,3 +3,77 @@
 Standard_Calculator::Standard_Calculator(QObject *parent)
     : QObject{parent}
 {}
+
+int Standard_Calculator::factorial(const int &a)
+{
+    if(a == 0){
+        return 1;
+    }
+    return a * this->factorial(a-1);
+}
+
+int Standard_Calculator::modulus(const int &a, const int &b)
+{
+    if(b == 0){
+        return NULL;
+    }
+    if(a == 0){
+        return 0;
+    }
+    else{
+        return a%b;
+    }
+}
+
+int Standard_Calculator::gcd(const int &a, const int &b)
+{
+
+}
+
+template<typename T>
+T Standard_Calculator::addition(const T &a, const T &b)
+{
+    return a + b;
+}
+
+template<typename T>
+T Standard_Calculator::subtraction(const T &a, const T &b)
+{
+    return a - b;
+}
+
+template<typename T>
+T Standard_Calculator::multiplication(const T &a, const T &b)
+{
+    return a * b;
+}
+
+template<typename T>
+T Standard_Calculator::division(const T &a, const T &b)
+{
+    if(b == 0){
+        return NULL;
+    }
+    if(a == 0){
+        return 0;
+    }
+    else{
+        return a - b;
+    }
+}
+
+template<typename T, int>
+T Standard_Calculator::exponential(const T &a, int  &p)
+{
+    if(p)
+    T c = a;
+    for(int i = 0; i < p; i++ ){
+        c = c*a;
+    }
+}
+
+template<typename T>
+T Standard_Calculator::squareRoot(const T &a)
+{
+
+}
--- standard_calculator/standard_calculator.h
+++ standard_calculator/standard_calculator.h
@@ -3,11 +3,46 @@
 
 #include <QObject>
 
+/*Standard Calculator that will implement
+* Addition
+* Subtraction
+* Multiplication
+* Division
+* Modulus
+* Exponents
+* Square Root
+*/
 class Standard_Calculator : public QObject
 {
     Q_OBJECT
 public:
     explicit Standard_Calculator(QObject *parent = nullptr);
+
+    template<typename T>
+    T addition(const T &a, const T &b);
+
+    template<typename T>
+    T subtraction(const T &a, const T &b);
+
+    template<typename T>
+    T multiplication(const T &a, const T &b);
+
+    template<typename T>
+    T division(const T &a, const T &b);
+
+    template<typename T>
+    T exponential(const T &a, const int &p);
+
+    template<typename T>
+    T squareRoot(const T &a);
+
+    int factorial(const int &a);
+
+    int modulus(const int &a, const int &b);
+
+    int gcd(const int &a, const int &b);
+
+private:
 
 signals:
 };
